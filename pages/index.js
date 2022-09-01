25f2bf57d10561f5b404c287ccc2e962dd8010bf
Stefan-migo <stefan.migo@gmail.com>
2022-09-01 19:34:02 -0400

    small change

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ products, bannerData }) => {
           <Product 
             key={product._id}
             product={product}
-          />
+          /> 
         )}
       </div>
 
