9e7327e7ad00c3d2b412ffe7294ef57970cc4f92
Stefan-migo <stefan.migo@gmail.com>
2022-09-02 11:37:24 -0400

    fixing stuff

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AiFillInstagram, AiOutlineGithub, AiOutlineLinkedin} from 'react-icons/ai';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
